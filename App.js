22a81d4fec5883dfe6289b97656ead2939d19c37
Numan Şenlik <nmnsnlk@gmail.com>
2024-02-12 23:58:05 +0300

    First basic form

--- App.js
+++ App.js
@@ -1,11 +1,47 @@
+// Status bar Ekranın üstünün temsilcisidir ve container ın stillerine tabi olmuyor
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
+// Text yazı yazmamızı sağlayan comp
+// view container olarak kullanılıyor
+// React fragment yerine view kullanılıyor
 
 export default function App() {
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [fullName, setFullName] = useState("");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.textStyle}>Name Surname: {fullName}</Text>
+      <Text style={styles.textStyle}>Name</Text>
+      <TextInput
+        placeholder="Enter Your Name"
+        value={name}
+        onChangeText={(i) => {
+          setName(i);
+        }}
+        style={styles.textInput}
+      />
+      <Text style={styles.textStyle}>Surname</Text>
+      <TextInput
+        placeholder="Enter Your Name"
+        value={surname}
+        onChangeText={(i) => {
+          setSurname(i);
+        }}
+        style={styles.textInput}
+      />
+      <Pressable
+        onPress={({ pressed }) => {
+          setFullName(name + surname);
+        }}
+      >
+        <Text style={styles.textInput}>Tiklayiniz</Text>
+      </Pressable>
       <StatusBar style="auto" />
+      {/* <View style={styles.subContainer}>
+        <Text style={styles.textStyle}>Test Text</Text>
+      </View> */}
     </View>
   );
 }
@@ -17,4 +53,32 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  subContainer: {
+    backgroundColor: "green",
+    fontSize: "20",
+    justifyContent: "center",
+    alignItems: "center",
+    height: 200,
+    width: "40%",
+    borderWidth: 6,
+    borderColor: "blue",
+    borderRadius: 40,
+  },
+  textStyle: {
+    fontSize: 20,
+    margin: 10,
+  },
+  textInput: {
+    backgroundColor: "lightgray",
+    width: "70%",
+    height: 30,
+    borderWidth: 0.5,
+    borderRadius: 10,
+    textAlign: "center",
+    justifyContent: "center",
+  },
 });
+
+// App.json dosyası proje bilgilerini tutar (version,ad)gibi özelliklerini tutar icon telefondaki uygulama
+// simgesini localdeki yerini belirtiyo
+// splash yüklenme ekranı
